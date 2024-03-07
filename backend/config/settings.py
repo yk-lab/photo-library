b932d26a910c5ad0fed74ea2ad06213d64bc7e95
yk-lab <yk-lab@users.noreply.github.com>
2024-03-08 01:08:36 +0900

    chore: setup admindocs

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -71,6 +71,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.admindocs",  # 追加
 ]
 if DEBUG:
     # DebugToolbarMiddleware の設定
@@ -86,6 +87,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    #
+    # django.contrib.admindocs
+    "django.contrib.admindocs.middleware.XViewMiddleware",
 ]
 if DEBUG:
     # DebugToolbarMiddleware の設定
